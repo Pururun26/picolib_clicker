9263a39c3f7ff6a51cf5077027f5467603ba3e1a
Pururun26 <pururunkun26@gmail.com>
2026-08-31 23:22:19 +0300

    Update picolib_conf.h

--- src/picolib_conf.h
+++ src/picolib_conf.h
@@ -7,8 +7,10 @@
 #define PICOLIB_TITLE   "PICOLIB CLICKER"
 #define PICOLIB_FPS     30
 
+
 // Путь и имя файла спрайт-листа (должен быть 128x128 пикселей для 256 спрайтов 8x8)
 #define PICOLIB_SS      "assets\\picolib_spritesheet.png"
+
 
 // Шрифт
 #define PICOLIB_FONT    "assets\\font_pico8.png"
@@ -17,7 +19,26 @@
 #define PICOLIB_USE_AUDIO       1                  // 1 включить звук для игры, 0 отключить звук
 #define PICOLIB_SOUNDS_PATH     "assets\\sounds/%d.wav"    // Путь к звуковым файлам
 
+
 // Мышка
 #define PICOLIB_USE_MOUSE       1
+
+
+// Сохранение и загрузка
+#define PICOLIB_USE_SAVE        1
+#define PICOLIB_SAVE_SLOTS      64
+#define PICOLIB_SAVE_FILE       "storage.bin"
+
+
+// Карта
+#define PICOLIB_USE_MAP         1
+
+// Размеры карты
+#define SPRITE_COUNT 256
+#define MAP_ROWS     32
+#define MAP_COLS     128
+
+// Путь к файлу карты (если используется)
+#define PICOLIB_MAP_FILE "map.csv"
 
 #endif
